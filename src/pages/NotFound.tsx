1c3b71802cd9ec6e27968d8d195853a845ca8302
akbar-99 <akbar-99@users.noreply.github.com>
2025-12-24 18:57:33 +0400

    Security: Remove 404 route logging to address information disclosure warning

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,7 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
 import SEO from "@/components/SEO";
 
 const NotFound = () => {
-  const location = useLocation();
 
-  useEffect(() => {
-    console.error("404 Error: User attempted to access non-existent route:", location.pathname);
-  }, [location.pathname]);
 
   return (
     <>
